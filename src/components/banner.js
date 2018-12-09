e992c6a6ecb52644203fff521918ca7048b5543d
arellanoelden <arellanoelden11@yahoo.com>
2018-12-08 20:43:14 -0800

    finish banner and edit navbar to be colored the same as header

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,13 +4,10 @@ class banner extends Component {
   render() {
     return(
       <section className="banner">
-        <picture>
-          <img alt="Elden Arellano" src={require("../images/banner.jpeg")} />
-        </picture>
         <span className="bannerInfo">
           <h1>Elden Arellano</h1>
-          <p>arellanoelden11@yahoo.com</p>
-          <p>Assoicate Front End Engineer</p>
+          <p className="email">arellanoelden11@yahoo.com</p>
+          <p className="occupation">Assoicate Front End Engineer</p>
         </span>
       </section>
     );
